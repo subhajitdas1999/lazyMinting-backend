84d24a53760375c34e8098276b517549fe9d63b8
Subhajit Das <sd651846@gmail.com>
2022-05-19 10:46:33 +0530

    initial error handler for development only

---
+++ controllers/globalErrorHandler.js
@@ -0,0 +1,22 @@
+const sendErrorDev = async (err, req, res) => {
+  //1 for apis
+  if (req.originalUrl.startsWith("/api")) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+};
+
+const globalErrorHandler = async (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, req, res);
+  }
+};
+
+export default  globalErrorHandler;
