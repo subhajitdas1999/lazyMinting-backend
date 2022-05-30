b86e18309448c69f9835474c0e0a72ba9824014b
Subhajit Das <sd651846@gmail.com>
2022-05-30 18:36:33 +0530

    added cloud db connection

--- server.js
+++ server.js
@@ -6,27 +6,30 @@ dotenv.config({ path: "./.env" });
 
 //suppose we have uncaught error (ex console.log(x))
 //We have to define this process.on before any exception ,any exception occurs after defining this process.on will be not handled by this handler
-process.on("uncaughtException",err => {
+process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   console.log("UNCAUGHT Exception");
-  
-    process.exit(1);
-})
 
-//local DB connection
-const DB = process.env.DB_LOCAL;
-
-//cloud db connection
-// const DB = process.env.DB_CLOUD.replace("<PASSWORD>", process.env.DB_PASSWORD);
-
-mongoose.connect(DB).then((connection) => {
-  console.log("DB connection is successful");
-})
-.catch((err) => {
-  console.log(err);
+  process.exit(1);
 });
 
-const PORT =process.env.PORT || 8000;
+//local DB connection
+// const DB = process.env.DB_LOCAL;
+
+//cloud db connection
+const DB = process.env.DB_CLOUD.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+
+mongoose
+  .connect(DB)
+  .then((connection) => {
+    console.log("DB connection is successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const PORT = process.env.PORT || 8000;
 
 const server = app.listen(PORT, () =>
   console.log(`starts listening on port ${PORT}`)
@@ -42,4 +45,3 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
-
